6d30484895c4ada970a63f5aafc7d6144b4c034f
Maxwell Kenned <kenned123@gmail.com>
2021-05-12 11:00:34 -0300

    add global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,16 +7,58 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html {
-    font-size: 62.5%;
+  :root {
+    --white: #fff;
+    --background: #F3F3F4;
+    --green-200: #77D400;
+    --green-400: #1BBB16;
+    --pink-800:#B41C8B;
+    --orange-800: #FF5959;
+    --gray-100: #ececec;
+    --gray-200: #E0E0E0;
+    --gray-300: #939393;
+    --gray-400: #898989;
+    --gray-500: #818181;
+    --gray-600: #3f3f3f;
+    --gray-800: #333;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%; // 15px
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%; // 15px
+    }
+  }
+
+  html, body {
+    overflow: hidden;
   }
 
   html, body, #__next {
-    height: 100%;
+    min-width: 100%;
+    min-height: 100vh;
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    background-color: var(--background);
+    font-family: Quicksand, sans-serif;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  button {
+    cursor: pointer;
   }
 `
 
