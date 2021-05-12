9f9977f152b60743c8365e0e8c4ad9ada21b9eae
Maxwell Kenned <kenned123@gmail.com>
2021-05-12 11:06:10 -0300

    add Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,50 @@
-import Main from 'components/Main'
+import { useEffect } from 'react'
 
-export default function Home() {
-  return <Main />
+import { CategoryList } from 'components/CategoryList'
+import { Header } from 'components/Header'
+import { ProductList } from 'components/ProductList'
+import { Search } from 'components/Search'
+import { useProduct } from 'contexts/Products'
+import { GetStaticProps } from 'next'
+import { IProduct } from 'types/IProduct'
+
+import api from 'api'
+
+import * as S from '../styles/home.styles'
+import { getAllProducts } from 'services/products'
+
+interface IHomeProps {
+  products: IProduct[]
+  categories: string[]
+}
+
+export default function Home({ products, categories }: IHomeProps) {
+  const { setProducts } = useProduct()
+
+  useEffect(() => {
+    setProducts(products)
+  }, [products, setProducts])
+
+  return (
+    <S.Wrapper>
+      <Header />
+      <CategoryList categories={categories} />
+      <S.Content>
+        <Search />
+        <ProductList />
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await getAllProducts()
+  const { data: categories } = await api.get('categories')
+
+  return {
+    props: {
+      products,
+      categories
+    }
+  }
 }
---
+++ src/styles/home.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  overflow-y: hidden;
+`
+
+export const Content = styled.div`
+  padding: 3.125rem 2.125rem;
+  height: calc(100vh - 260px);
+  overflow-y: auto;
+`
